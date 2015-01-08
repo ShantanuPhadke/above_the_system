7aa359b4d3774de214022ced39c3aa9a47db340f
ShantanuPhadke <shantanu.cricket@gmail.com>
2015-01-08 13:21:24 -0800

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about_us" do
     get :about_us
     assert_response :success
+    assert_select "title", "About Us | Ruby on Rails Tutorial Sample App"
   end
 
 end
